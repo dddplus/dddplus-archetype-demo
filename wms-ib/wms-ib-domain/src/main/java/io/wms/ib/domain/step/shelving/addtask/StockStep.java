91f5e47d014d37a8f271269f66cf3cb970abe39f
Funky Gao <funky.gao@gmail.com>
2020-10-28 21:41:21 +0800

    fix gitignore

---
+++ wms-ib/wms-ib-domain/src/main/java/io/wms/ib/domain/step/shelving/addtask/StockStep.java
@@ -0,0 +1,20 @@
+package io.wms.ib.domain.step.shelving.addtask;
+
+import io.github.dddplus.annotation.Step;
+import io.wms.ib.domain.model.ShelvingTask;
+import io.wms.ib.domain.step.shelving.AddShelvingTaskStep;
+import io.wms.ib.spec.Steps;
+import io.wms.ib.spec.ext.IbException;
+
+@Step
+public class StockStep extends AddShelvingTaskStep {
+
+    @Override
+    public void execute(ShelvingTask model) throws IbException {
+    }
+
+    @Override
+    public String stepCode() {
+        return Steps.AddShelvingTask.StepCode.Stock;
+    }
+}
---
+++ wms-ib/wms-ib-domain/src/main/java/io/wms/ib/domain/step/shelving/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 上架任务相关的领域步骤.
+ */
+package io.wms.ib.domain.step.shelving;
