8c57d33d6e1ecc4d1bfa5961450b4e2b386b84b5
Funky Gao <funky.gao@gmail.com>
2020-10-28 22:44:13 +0800

    提供默认的步骤编排逻辑

--- wms-ib/wms-ib-domain/src/main/java/io/wms/ib/domain/ability/DecideStepsAbility.java
+++ wms-ib/wms-ib-domain/src/main/java/io/wms/ib/domain/ability/DecideStepsAbility.java
@@ -4,14 +4,19 @@ import io.github.dddplus.annotation.DomainAbility;
 import io.github.dddplus.ext.IDecideStepsExt;
 import io.github.dddplus.runtime.BaseDomainAbility;
 import io.wms.ib.domain.CoreDomain;
+import io.wms.ib.domain.ability.extension.DefaultDecideStepsExt;
 import io.wms.ib.spec.DomainAbilities;
 import io.wms.ib.spec.model.IShelvingTask;
 
+import javax.annotation.Resource;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@DomainAbility(domain = CoreDomain.CODE, name = "动态决定领域步骤的能力", tags = DomainAbilities.decideSteps)
+@DomainAbility(domain = CoreDomain.CODE, tags = DomainAbilities.decideSteps)
 public class DecideStepsAbility extends BaseDomainAbility<IShelvingTask, IDecideStepsExt> {
+
+    @Resource
+    private DefaultDecideStepsExt defaultDecideStepsExt;
 
     public List<String> decideSteps(@NotNull IShelvingTask model, String activityCode) {
         return firstExtension(model).decideSteps(model, activityCode);
@@ -19,6 +24,6 @@ public class DecideStepsAbility extends BaseDomainAbility<IShelvingTask, IDecide
 
     @Override
     public IDecideStepsExt defaultExtension(IShelvingTask model) {
-        return null;
+        return defaultDecideStepsExt;
     }
 }
---
+++ wms-ib/wms-ib-domain/src/main/java/io/wms/ib/domain/ability/extension/DefaultDecideStepsExt.java
@@ -0,0 +1,33 @@
+package io.wms.ib.domain.ability.extension;
+
+import io.github.dddplus.annotation.Extension;
+import io.github.dddplus.ext.IDecideStepsExt;
+import io.github.dddplus.model.IDomainModel;
+import io.wms.ib.spec.Steps;
+
+import java.util.*;
+
+@Extension(code = IDecideStepsExt.DefaultCode)
+public class DefaultDecideStepsExt implements IDecideStepsExt {
+    private static final List<String> emptySteps = Collections.emptyList();
+
+    @Override
+    public List<String> decideSteps(IDomainModel model, String activityCode) {
+        List<String> steps = stepsRegistry.get(activityCode);
+        if (steps == null) {
+            return emptySteps;
+        }
+
+        return steps;
+    }
+
+    private static Map<String, List<String>> stepsRegistry = new HashMap<>();
+    static {
+        List<String> addShelvingTaskSteps = new ArrayList<>();
+        addShelvingTaskSteps.add(Steps.AddShelvingTask.StepCode.NoConcurrency);
+        addShelvingTaskSteps.add(Steps.AddShelvingTask.StepCode.Stock);
+        addShelvingTaskSteps.add(Steps.AddShelvingTask.StepCode.Recommend);
+        addShelvingTaskSteps.add(Steps.AddShelvingTask.StepCode.Persist);
+        stepsRegistry.put(Steps.AddShelvingTask.Activity, addShelvingTaskSteps);
+    }
+}
